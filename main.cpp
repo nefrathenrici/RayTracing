2a3ddb05c6913c755a243a3d19c529d1a012c1a3
nefrathenrici <nat.henrici@gmail.com>
2020-12-18 21:46:48 -0500

    Finished 6.12

--- main.cpp
+++ main.cpp
@@ -5,10 +5,33 @@
 #include "ray.hpp"
 #include "vec3.hpp"
 
+double hit_sphere(const point3& center, double radius, const ray& r) {
+    // Detects if the ray r collides with the sphere given by center and radius
+    vec3 o_minus_c = r.origin() - center;
+    double a = r.direction().length_squared();
+    double half_b = dot(o_minus_c, r.direction());
+    double c = o_minus_c.length_squared() - radius*radius;
+    // Put discrminant together
+    double discriminant = half_b*half_b - a*c;
+    // If disc < 0, no real roots, doesn't intersect w/ circle
+    if (discriminant < 0) {
+        return -1.0;
+    } else {
+        // Quadratic formula - assume unit vector
+        return (-half_b - sqrt(discriminant) ) / a;
+    }
+}
+
 color ray_color(const ray& r) {
+    double t = hit_sphere(point3(0,0,-1), 0.5, r);
+    if (t > 0.0) {
+        // N is normal vector
+        vec3 N = unit_vector(r.at(t) - vec3(0,0,-1));
+        return 0.5*color(N.x()+1, N.y()+1, N.z()+1);
+    }
     vec3 unit_direction = unit_vector(r.direction());
-    auto t = 0.5*(unit_direction.y() + 1.0);
-    // Currently hardcoded values to light blue
+    t = 0.5*(unit_direction.y() + 1.0);
+    // Currently hardcodes gradient values to light blue
     return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
 }
 
